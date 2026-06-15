8a184d2580a3129b24478f304deb74072d71911c
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2026-06-14 17:54:34 -0700

    More collaborative delta functions

--- delta.py
+++ delta.py
@@ -1,28 +1,4 @@
-'''
-Created on Jun 9, 2026
-
-@author: bcrow
-'''
-
-'''
-def shift(src, shift):
-    dim  = src.shape
-    ydim = dim[0]
-    xdim = dim[1]
-
-    dst  = src.copy()
-
-    
-    if(shift < 0):
-        for i in range(0, ydim):
-            for j in range(0, xdim): 
-                dst[i, j] >>= -shift
-    else:
-        for i in range(0, ydim):
-            for j in range(0, xdim): 
-                dst[i, j] <<= shift
-    return dst
-'''
+import numpy as np
 
 def shift(src, shift):
     dst    = src.copy()
@@ -33,4 +9,134 @@ def shift(src, shift):
     else:
         dst *= factor
     
+    return dst
+
+def get_vertical_deltas_from_values(src: np.ndarray, xdim: int, ydim: int) -> tuple:
+    dst = np.zeros(xdim * ydim, dtype=np.int32)
+    init_value = src[0]
+    value = init_value
+    sum_ = 0
+
+    k = 0
+    for i in range(ydim):
+        for j in range(xdim):
+            if i == 0:
+                if j == 0:
+                    dst[k] = 0
+                    k += 1
+                else:
+                    delta = src[k] - value
+                    value += delta
+                    dst[k] = delta
+                    k += 1
+                    sum_ += abs(delta)
+            else:
+                delta = src[k] - src[k - xdim]
+                dst[k] = delta
+                k += 1
+                sum_ += abs(delta)
+
+    return sum_, dst, init_value
+
+
+def get_values_from_vertical_deltas(src: np.ndarray, xdim: int, ydim: int, init_value: int) -> np.ndarray:
+    dst = np.zeros(xdim * ydim, dtype=np.int32)
+    dst[0] = init_value
+    value = init_value
+
+    for i in range(1, xdim):
+        value += src[i]
+        dst[i] = value
+
+    for i in range(1, ydim):
+        for j in range(xdim):
+            index = i * xdim + j
+            dst[index] = dst[index - xdim] + src[index]
+
+    return dst
+
+def get_vertical_deltas_from_values2(src: np.ndarray, xdim: int, ydim: int) -> tuple:
+
+    dst = np.zeros(xdim * ydim, dtype=np.int32)
+
+    init_value = src[0]
+
+    value = init_value
+
+    sum_ = 0
+
+
+
+    k = 0
+
+    for i in range(ydim):
+
+        for j in range(xdim):
+
+            if i == 0:
+
+                if j == 0:
+
+                    dst[k] = 0
+
+                    k += 1
+
+                else:
+
+                    delta = src[k] - value
+
+                    value += delta
+
+                    dst[k] = delta
+
+                    k += 1
+
+                    sum_ += abs(delta)
+
+            else:
+
+                delta = src[k] - src[k - xdim]
+
+                dst[k] = delta
+
+                k += 1
+
+                sum_ += abs(delta)
+
+
+
+    return sum_, dst, init_value
+
+
+
+
+
+def get_values_from_vertical_deltas2(src: np.ndarray, xdim: int, ydim: int, init_value: int) -> np.ndarray:
+
+    dst = np.zeros(xdim * ydim, dtype=np.int32)
+
+    dst[0] = init_value
+
+    value = init_value
+
+
+
+    for i in range(1, xdim):
+
+        value += src[i]
+
+        dst[i] = value
+
+
+
+    for i in range(1, ydim):
+
+        for j in range(xdim):
+
+            index = i * xdim + j
+
+            dst[index] = dst[index - xdim] + src[index]
+
+
+
     return dst
